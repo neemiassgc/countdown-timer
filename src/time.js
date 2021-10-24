3d1d68fa0581f4c9243740e7004e60772f449d6b
neemiassgc <neemiassgc@outlook.com>
2021-10-23 22:58:33 -0300

    Add time script

---
+++ src/time.js
@@ -0,0 +1,20 @@
+export function parseDateTimeToMillis(date, time) {
+	const [year, month, day] = date.split("-").map(i => +i);
+	const [hours, minutes] = time.split(":").map(i => +i);
+
+	return new Date(year, month - 1, day, hours, minutes).getTime()
+}
+
+export function formatTime(millis) {
+    const days = Math.floor(millis / 864e5)
+    const hours = Math.floor(millis / 36e5 % 24)
+    const minutes = Math.floor(millis / 6e4 % 60)
+    const seconds = Math.floor(millis / 1e3 % 60)
+
+    return {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    }
+}
