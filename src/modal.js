994f5f7cb303908c89e23ab77acaee94b4e33995
Neemias Santos <neemiassgc@outlook.com>
2021-11-16 00:17:32 -0300

    Add one more React component to the modal file: Prompt

--- src/modal.js
+++ src/modal.js
@@ -52,3 +52,20 @@ function Warning(props) {
         <Modal buttons={buttons} msg={props.msg}/>
     );
 }
+
+function Prompt(props) {
+    const buttons = (
+        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button onClick={this.props.toggle} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                OK
+            </button>
+            <button onClick={this.props.comfirm} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                OK
+            </button>
+        </div>
+    );
+
+    return (
+        <Modal buttons={buttons} msg={props.msg}/>
+    );
+}
