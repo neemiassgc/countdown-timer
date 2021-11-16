6f3185218da7edb884c3aed6c2998fd7c13f6c6f
Neemias Santos <neemiassgc@outlook.com>
2021-11-15 23:56:35 -0300

    add one more React component to the modal file: Modal

--- src/modal.js
+++ src/modal.js
@@ -7,3 +7,34 @@ function ExclamationSVG(props) {
         </svg>
     )
 }
+
+function Modal(props) {
+    return (
+        <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+        
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+    
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <ExclamationSVG/>
+                        </div>
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                Warning
+                            </h3>
+                            <div className="mt-2">
+                                <p className="text-md font-black text-gray-500">{props.msg}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {props.buttons}
+            </div>
+            </div>
+        </div>
+    )
+}
