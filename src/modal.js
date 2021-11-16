8149910f64d248addcc1014b013a0a3df7bd4d71
Neemias Santos <neemiassgc@outlook.com>
2021-11-16 11:19:06 -0300

    Center the position of the warning modal on the screen

--- src/modal.js
+++ src/modal.js
@@ -11,7 +11,7 @@ function ExclamationSVG(props) {
 function Modal(props) {
     return (
         <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="flex items-center justify-center min-h-screen pt-4 px-4 text-center sm:block sm:p-0">
                 <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
         
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
