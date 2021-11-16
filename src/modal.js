ec981ab13f4e96b7649aba1292bb05def6d167eb
Neemias Santos <neemiassgc@outlook.com>
2021-11-16 00:18:27 -0300

    Export two components from the modal file

--- src/modal.js
+++ src/modal.js
@@ -39,7 +39,7 @@ function Modal(props) {
     )
 }
 
-function Warning(props) {
+export function Warning(props) {
     const buttons = (
         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button onClick={this.props.toggle} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
@@ -53,7 +53,7 @@ function Warning(props) {
     );
 }
 
-function Prompt(props) {
+export function Prompt(props) {
     const buttons = (
         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button onClick={this.props.toggle} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
